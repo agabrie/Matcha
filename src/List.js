fbb37765fc349fa25f33ba6c511a665bc5945cf7
abduraghmaan GABRIELS <agabrie@c1r2s3.wethinkcode.co.za>
2018-12-11 16:16:29 +0200

    can switch between images to view

--- src/List.js
+++ src/List.js
@@ -34,7 +34,35 @@ class Username extends React.Component
 		);
 	}
 }
-class PP extends React.Component
+class MainImage extends React.Component
+{
+	constructor(props){
+		super(props);
+		this.state = {
+			value:this.props.value.pp1,
+		};
+	}
+	render(){
+		return(
+			<div>
+			<img className="info mainImage"
+				src={this.state.value}
+				alt=''
+			/>
+			<table>
+				{/* <tr> */}
+					<td><img className="info tiny"src={this.props.value.pp1} onClick={()=>this.setState({value:this.props.value.pp1})} /></td>
+					<td><img className="info tiny"src={this.props.value.pp2} onClick={()=>this.setState({value:this.props.value.pp2})} /></td>
+					<td><img className="info tiny"src={this.props.value.pp3} onClick={()=>this.setState({value:this.props.value.pp3})} /></td>
+					<td><img className="info tiny"src={this.props.value.pp4} onClick={()=>this.setState({value:this.props.value.pp4})} /></td>
+					<td><img className="info tiny"src={this.props.value.pp5} onClick={()=>this.setState({value:this.props.value.pp5})} /></td>
+				{/* </tr> */}
+			</table>
+			</div>
+		);
+	}
+}
+class Images extends React.Component
 {
 	constructor(props){
 		super(props);
@@ -44,10 +72,40 @@ class PP extends React.Component
 	}
 	render(){
 		return(
-			<img className="info pp"
-				src={this.props.value}
-				alt=''
-			/>
+			// <div className="info">
+						<table>
+							{/* <tr> */}
+								<td><img className="info tiny"src={this.props.value.pp1} onClick={()=>this.setState({value:this.src})} /></td>
+								<td><img className="info tiny"src={this.props.value.pp2} onClick={()=>this.setState({value:this.src})} /></td>
+								<td><img className="info tiny"src={this.props.value.pp3} onClick={()=>this.setState({value:this.src})} /></td>
+								<td><img className="info tiny"src={this.props.value.pp4} onClick={()=>this.setState({value:this.src})} /></td>
+								<td><img className="info tiny"src={this.props.value.pp5} onClick={()=>this.setState({value:this.src})} /></td>
+							{/* </tr> */}
+						</table>
+			// </div>
+		);
+	}
+}
+class Tags extends React.Component
+{
+	constructor(props){
+		super(props);
+		this.state = {
+			value:this.props,
+		};
+	}
+	render(){
+		return(
+			<div className='info tags'>
+			Tags :
+					{this.props.value.map(tag =>(
+						<div>
+							<a href=''>{'#'+tag}</a>
+						</div>
+					)
+					)
+					}
+			</div>
 		);
 	}
 }
@@ -68,6 +126,25 @@ class Gender extends React.Component
 		);
 	}
 }
+class Bio extends React.Component
+{
+	constructor(props){
+		super(props);
+		this.state = {
+			value:this.props,
+		};
+	}
+	render(){
+		return(
+			<div className='info bio'>
+			Bio :
+			<p>
+				{this.props.value}
+			</p>
+			</div>
+		);
+	}
+}
 
 class GenderBlob extends React.Component
 {
@@ -84,20 +161,13 @@ class GenderBlob extends React.Component
 		}
 	  return '#'+hex;
 	}
-
-	constructor(props){
-		super(props);
-		this.state = {
-			value:this.props,
-		};
-	}
-	generateColor(i)
+generateColor(i)
 	{
 		// ff99cc pink
 		// cc66ff purple
 		// 33ccff blue
 		var baser, baseg, baseb, r, b, g;
-		if(i <= 0.5)
+		if(i < 0.5)
 		{
 			baser = 0xff;
 			baseg = 0x99;
@@ -111,14 +181,21 @@ class GenderBlob extends React.Component
 			baseg = 0x66;
 			baseb = 0xff;
 		
-			r = baser - parseInt((i) * 153);
-			g = baseg + parseInt((i) * 102);
-			b = baseb + parseInt((i) * 0);
+			r = baser - parseInt((i-0.5) * (0xcc-0x33)); //153
+			g = baseg + parseInt((i-0.5) * 102);
+			b = baseb + parseInt((i-0.5) * 0);
 		}
 		var rgb = (r<<16) | (g <<8) | b;
 		// alert(this.decimalToHex(rgb));
 		return(this.decimalToHex(rgb));
 	}
+	constructor(props){
+		super(props);
+		this.state = {
+			value:this.props,
+		};
+	}
+	
 	render(){
 		return(
 			// <div className="info">
@@ -137,12 +214,18 @@ class List extends React.Component {
 				return <Username value={val} />;
 			case "Age":
 				return <Age value={val} />;
-			case 'PP':
-				return <PP value={val} />;
+			case 'MainImage':
+				return <MainImage value={val} />;
+			case 'Images':
+				return <Images value={val} />;
 			case 'Gender':
 				return <Gender value={val} />;
 			case 'GenderBlob':
 				return <GenderBlob value={val} />;
+			case 'Bio':
+				return <Bio value={val} />;
+			case 'Tags':
+				return <Tags value={val} />;
 			default:
 				break;
 		}
@@ -157,9 +240,16 @@ class List extends React.Component {
 				"age":38,
 				"gender":0.5,
 				"pref":0.81,
-				"pp":require('./nanana.jpg'),
+				// "images":require('./nanana.jpg'),
+				"images": {	pp1:require("./nanana.jpg"),
+							pp2:require("./roses.png"),
+							pp3:require("./avaj_uml.jpg"),
+							pp4:require("./apicture.png"),
+							pp5:require("./kawaii_neko.png")
+						},
 				"gps_lat":-60.043,
 				"gps_lon":-64.124,
+				"tags":"nana tags aloha quay",
 				"likes":"Harum corporis quis est labore consequatur totam. Possimus aut quam sed. Doloribus et aspernatur vero numquam ducimus officia. Totam reiciendis quae rerum eaque."
 			}
 			// 4.675680253925442,
@@ -168,7 +258,6 @@ class List extends React.Component {
 			// 6092.92,
 			// 0
 		];
-		
 	  return(
 				dun.map( dundun => (
 				// this.renderDiv("Username",dundun.user_name)
@@ -176,9 +265,12 @@ class List extends React.Component {
 					{this.renderDiv("Username",dundun.user_name)}
 					{this.renderDiv("Age",dundun.age)}
 					<div className="Central">
-						{this.renderDiv("PP",dundun.pp)}
+						{this.renderDiv("MainImage",dundun.images)}
+						{/* {this.renderDiv("Images",dundun.images)} */}
 						{this.renderDiv("Gender",dundun.gender)}
 						{this.renderDiv("GenderBlob",dundun.gender)}
+						{this.renderDiv("Bio",dundun.likes)}
+						{this.renderDiv("Tags",dundun.tags.split(' '))}
 					</div>
 				</div>
 				// <div>{dundun.user_name} </div>
