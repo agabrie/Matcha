28821fa185dc6a2312d7489c8d63799029676927
agabrie <abduraghmaangabriels2@gmail.com>
2020-01-15 17:18:06 +0200

    added COR support

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ mongoose.connect(url, {
 
 mongoose.Promise = global.Promise;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(bodyParser.json());
 
 app.use('/api', require('./routes/api'));
