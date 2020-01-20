9c653ddd926f6ec95affc17a3cb0745ef147fe0a
kheynes <heyneskeziah@gmail.com>
2020-01-20 13:49:11 +0200

    did some front end stuff

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/layout/Header';
+import Sign from './components/pages/Sign';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Sign} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ front_end/src/components/pages/Sign.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sign() {
+    return(
+        <React.Fragment>
+            <p> This will be a sign in page </p>
+        </React.Fragment>
+    )
+}
+
+export default Sign;
